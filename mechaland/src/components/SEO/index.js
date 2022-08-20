fa6885138bae417c76bb903382d7e466ccb1d5fb
Koma Studio <komastudio.tech@gmail.com>
2022-08-20 08:04:21 +0700

    update

--- mechaland/src/components/SEO/index.js
+++ mechaland/src/components/SEO/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Script from "next/script";
 
 const defaultValue = {
   title:
@@ -24,42 +25,55 @@ const SEO = ({
   type,
 }) => {
   return (
-    <Head>
-      <title>{title ?? defaultValue.title}</title>
-      <meta name="title" content={title ?? defaultValue.title} />
-      <meta
-        name="description"
-        content={description ?? defaultValue.description}
-      />
-      <meta name="author" content={author ?? defaultValue.author} />
-      <meta name="copyright" content={copyright ?? defaultValue.copyright} />
-      <a rel="Syklik" href="/favicon.ico" />
-      <a rel="canonical" href={cannonical ?? defaultValue.cannonical} />
-      <meta name="robots" content="index,follow" />
-      <meta property="og:title" content={title ?? defaultValue.title} />
-      <meta
-        property="og:site_name"
-        content={siteName ?? defaultValue.siteName}
-      />
-      <meta property="og:url" content={url ?? defaultValue.url} />
-      <meta
-        property="og:description"
-        content={description ?? defaultValue.description}
-      />
-      <meta property="og:type" content={type ?? defaultValue.type} />
-      <meta name="twitter:card" content="summary" />
-      <meta property="twitter:url" content={url ?? defaultValue.url} />
-      <meta name="twitter:title" content={title ?? defaultValue.title} />
-      <meta
-        name="twitter:description"
-        content={description ?? defaultValue.description}
-      />
-      <meta name="viewport" content="initial-scale = 1.0,maximum-scale = 1.0" />
-      <a rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" />
+    <>
+      <Head>
+        <title>{title ?? defaultValue.title}</title>
+        <meta name="title" content={title ?? defaultValue.title} />
+        <meta
+          name="description"
+          content={description ?? defaultValue.description}
+        />
+        <meta name="author" content={author ?? defaultValue.author} />
+        <meta name="copyright" content={copyright ?? defaultValue.copyright} />
+        <a rel="Syklik" href="/favicon.ico" />
+        <a rel="canonical" href={cannonical ?? defaultValue.cannonical} />
+        <meta name="robots" content="index,follow" />
+        <meta property="og:title" content={title ?? defaultValue.title} />
+        <meta
+          property="og:site_name"
+          content={siteName ?? defaultValue.siteName}
+        />
+        <meta property="og:url" content={url ?? defaultValue.url} />
+        <meta
+          property="og:description"
+          content={description ?? defaultValue.description}
+        />
+        <meta property="og:type" content={type ?? defaultValue.type} />
+        <meta name="twitter:card" content="summary" />
+        <meta property="twitter:url" content={url ?? defaultValue.url} />
+        <meta name="twitter:title" content={title ?? defaultValue.title} />
+        <meta
+          name="twitter:description"
+          content={description ?? defaultValue.description}
+        />
+        <meta name="viewport" content="initial-scale = 1.0,maximum-scale = 1.0" />
+
+        {/* <a rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" />
       <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
       <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
-    </Head>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script> */}
+      </Head>
+      {/* <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+        crossOrigin="anonymous"
+      />
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+        crossOrigin="anonymous" /> */}
+
+    </>
   )
 }
 
--- mechaland/src/pages/join/index.js
+++ mechaland/src/pages/join/index.js
@@ -10,58 +10,59 @@ import Link from 'next/link';
 
 export default function Join() {
 
-  return (
-    <div className={styles.container}>
-      <SEO
-        title="How To Join | Mechaland"
-        description="Mechaland"
-      />
+	return (
+		<div className={styles.container}>
+			<SEO
+				title="How To Join | Mechaland"
+				description="Mechaland"
+			/>
 
-      <main className={styles.main}>
-        <div className={styles.description} style={{marginBottom: "10px", paddingBottom: "5px"}}>
-          <div className={styles.desc}>
-            <h1 className={styles.title}>How To Join</h1>
-        	<Divider className={styles.dividerJoin} />
-          </div>
-        </div>
-        <div className={styles.section}>
-			<h5 className={styles.joinStep}>STEP 1:</h5>
-			<Image  width='672' height='350' src="/assets/join/qk60_how_to.png" alt="Mechaland Join 1" className={styles.joinPict}/><br/>
-			<a style={{marginBottom: "15px"}}>Click the green button which leads to our official Discord</a><br/>
-			<a>or</a><br/>
-			<Link href="https://discord.com/invite/Y5Bj6rK9He"><a target="_blank" rel="noopener noreferrer" className={`btn btn-lg active ${styles.discordButton}`} role="button" aria-pressed="true" style={{marginBottom: "50px"}}>
-				<Row>
-					<Col>
-					<Image  width='45' height='45' src="/assets/discord_white.png" alt="Mechaland Discord" className={styles.discordImage}/>
-					</Col>
-					<Col>
-					<div className={styles.discordJoin}>JOIN OUR</div><div className={styles.discordDC}>DISCORD</div>
-					</Col>
-				</Row>
-			</a></Link>
+			<main className={styles.main}>
+				<div className={styles.description} style={{ marginBottom: "10px", paddingBottom: "5px" }}>
+					<div className={styles.desc}>
+						<h1 className={styles.title}>How To Join</h1>
+						<Divider className={styles.dividerJoin} />
+					</div>
+				</div>
+				<div className={styles.section}>
+					<h5 className={styles.joinStep}>STEP 1:</h5>
+					{/* <Image width='672' height='350' src="/assets/join/qk60_how_to.png" alt="Mechaland Join 1" className={styles.joinPict} /><br /> */}
+					<a style={{ marginBottom: "15px" }}>Click the green button which leads to our official Discord</a><br />
+					{/* <a>or</a><br /> */}
+					<Link href="https://discord.com/invite/Y5Bj6rK9He"><a target="_blank" rel="noopener noreferrer" className={`btn btn-lg active ${styles.discordButton}`} role="button" aria-pressed="true" style={{ marginBottom: "50px" }}>
+						<Row>
+							<Col>
+								<Image width='45' height='45' src="/assets/discord_white.png" alt="Mechaland Discord" className={styles.discordImage} />
+							</Col>
+							<Col>
+								<div className={styles.discordJoin}>JOIN OUR</div><div className={styles.discordDC}>DISCORD</div>
+							</Col>
+						</Row>
+					</a></Link>
 
-			<h5 className={styles.joinStep}>STEP 2:</h5>
-			<Image  width='320' height='350' src="/assets/join/join2.png" alt="Mechaland Join 2" className={styles.joinPict} style={{maxWidth: "25vw"}}/><br/>
-			<a>Click Join button</a><br/><br/><br/>
+					<h5 className={styles.joinStep}>STEP 2:</h5>
+					<Image width='320' height='350' src="/assets/join/join2.png" alt="Mechaland Join 2" className={styles.joinPict} style={{ maxWidth: "25vw" }} /><br />
+					<a>Click Join button</a><br /><br /><br />
 
-			<h5 className={styles.joinStep}>STEP 3:</h5>
-			<Image  width='1000' height='550' src="/assets/join/join3.png" alt="Mechaland Join 3" className={styles.joinPict} style={{maxWidth: "80vw"}}/><br/>
-			<a>Click the Check mark icon and also Complete button to gain access to the server</a><br/><br/><br/>
+					<h5 className={styles.joinStep}>STEP 3:</h5>
+					<Image width='1000' height='550' src="/assets/join/join3.png" alt="Mechaland Join 3" className={styles.joinPict} style={{ maxWidth: "80vw" }} /><br />
+					<a>Click the Check mark icon and also Complete button to gain access to the server</a><br /><br /><br />
 
-			<h5 className={styles.joinStep}>STEP 4:</h5>
-			<Image  width='450' height='450' src="/assets/join/join4.png" alt="Mechaland Join 4" className={styles.joinPict} /><br/>
-			<a>Check the “I have read and agree to the rules” box, and click submit.</a><br/><br/><br/>
+					<h5 className={styles.joinStep}>STEP 4:</h5>
+					<Image width='450' height='450' src="/assets/join/join4.png" alt="Mechaland Join 4" className={styles.joinPict} /><br />
+					<a>Check the “I have read and agree to the rules” box, and click submit.</a><br /><br /><br />
 
-			<h5 className={styles.joinStep}>STEP 5:</h5>
-			<Image  width='270' height='1000' src="/assets/join/join5.png" alt="Mechaland Join 5" className={styles.joinPict} /><br/>
-			<a>Click Interest-Check</a><br/><br/><br/>
+					<h5 className={styles.joinStep}>STEP 5:</h5>
+					<Image width='270' height='1000' src="/assets/join/join5.png" alt="Mechaland Join 5" className={styles.joinPict} /><br />
+					<a>Click Interest-Check</a><br /><br /><br />
 
-			<h5 className={styles.joinStep}>STEP 6:</h5>
-			<Image  width='1000' height='550' src="/assets/join/join6.png" alt="Mechaland Join 6" className={styles.joinPict} style={{maxWidth: "80vw"}}/><br/>
-			<a>Click Interest-Check and follow the instruction on joining or buying the product(s) or join our discord server by clicking the button below.</a><br/><br/>
-			<iframe src="https://discord.com/widget?id=951980041812729986&theme=dark" width="450" height="600" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
-        </div>
-      </main>
-    </div>
-  )
+					<h5 className={styles.joinStep}>STEP 6:</h5>
+					<Image width='1000' height='550' src="/assets/join/join6.png" alt="Mechaland Join 6" className={styles.joinPict} style={{ maxWidth: "80vw" }} /><br />
+					<a>Click Interest-Check and follow the instruction on joining or buying the product(s).</a>
+					{/* <br /><br /> */}
+					{/* <iframe src="https://discord.com/widget?id=951980041812729986&theme=dark" width="450" height="600" allowtransparency="true" frameBorder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe> */}
+				</div>
+			</main>
+		</div>
+	)
 }
